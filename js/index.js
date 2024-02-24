8ccad6681d239fbb83251f960b03667cf5e1b210
Mariem Abdalwahab <mariem2562003@gmail.com>
2024-02-24 04:16:59 +0200

    image slider

--- js/index.js
+++ js/index.js
@@ -21,50 +21,98 @@ let imgs = [
 let pre = document.querySelector('.pre');
 let next = document.querySelector('.next');
 let pagin = document.querySelector('.pagin');
+let num = document.querySelector('.imgs span');
 
-let imgindex = 1;
+
+
+let imgindex = 0;
 let img = document.querySelector('.imgs img');
 img.src = imgs[imgindex].src;
+num.innerHTML = imgs[imgindex].id
 // creat paginantion
 
 function creat(imgs){
     return imgs.map(img=>{
-        return `<li data-id=${img.id}>${img.id}</li>`
+        if(img.id != 1){
+            return `<li data-id=${img.id-1} class="li not-active">${img.id}</li>`
+        } else{
+            return `<li data-id=${img.id-1} class="li active">${img.id}</li>`
+        }
     }).join('');
 
 }
 pagin.innerHTML = creat(imgs);
-let lis = document.querySelectorAll('.pagin li');
-lis = Array.from(lis);
+let lis = [...document.querySelectorAll('.pagin li')];
 
-
-pre.addEventListener('click',main);
-next.addEventListener('click',main);
-
-lis.map(li => {
-    li.addEventListener("click",main);
-});
-
-function main(){
-    if(this.classList.contains('pre')){
-        imgindex--;
-        if(imgindex<0){
-            imgindex = imgs.length-1;
+// active btns
+function activeLi(){
+    lis.forEach(li=>{
+        if(li.dataset.id == imgindex ){
+            li.classList.add('active')
+        } else{
+            li.classList.remove('active')
         }
+    })
+}
+activeLi();
 
-    }
-    else if(this.classList.contains('next')){
-        imgindex++;
-        if(imgindex==imgs.length){
-            imgindex = 0;
-        }
+
+
+pre.addEventListener('click',(e)=>{
+    if (imgindex == 0) {
+        pre.classList.add("notActive");
+        next.classList.remove("notActive");
+        
     }
     else{
-        imgindex = this.dataset.id;
         imgindex--;
-        console.log(imgindex);
+        main(e.target);
     }
+});
 
-    img.src = imgs[imgindex].src;
-    console.log(img.id)
+next.addEventListener('click',(e)=>{
+    if(imgindex == imgs.length-1){
+        pre.classList.remove("notActive");
+        next.classList.add("notActive");
+        
+    } else{
+        imgindex++;
+        main(e.target);
+    }
+});
+
+lis.map(li => {
+    li.addEventListener("click",(e)=>{
+        main(e.target);
+    });
+});
+
+function main(type){
+    
+    if(type.classList.contains("li")){
+        img.src = imgs[type.dataset.id].src;
+        num.innerHTML = imgs[type.dataset.id].id
+        imgindex = type.dataset.id;
+    }else{
+        img.src = imgs[imgindex].src;
+        num.innerHTML = imgs[imgindex].id
+    }
+    check();
+    activeLi();
+   
 }
+
+function check(){
+    if (imgindex == 0) {
+        next.classList.remove("notActive");
+        pre.classList.add("notActive");
+        return 0;
+    } else if(imgindex == imgs.length-1){
+        pre.classList.remove("notActive");
+        next.classList.add("notActive");
+        return 0;
+    }
+    return 1;
+}
+
+check()
